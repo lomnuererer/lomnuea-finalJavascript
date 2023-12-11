159cf9f1c784c1068ceceea9defe9e040cdc066c
“lomnuererer” <hros8309@gmail.com>
2023-12-11 20:46:08 +0700

    90%

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Load CSS
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    // link.href = 'style.css';
+    link.href = 'style.css';
     document.head.appendChild(link);
 
     // Load Navbar
@@ -11,5 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
+
+            // Add event listener to the navbar-toggler button
+            const navbarToggler = document.querySelector('.navbar-toggler');
+            const navbarCollapse = document.querySelector('.navbar-collapse');
+
+            navbarToggler.addEventListener('click', function () {
+                navbarCollapse.classList.toggle('show');
+            });
         });
 });
+
